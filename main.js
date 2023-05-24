c98992cb9abb8b5428d1f7b25fb911ce15d84414
Yara Amged <Yara.Amged@itworx.com>
2023-05-25 00:02:19 +0300

    Updated

--- main.js
+++ main.js
@@ -8,39 +8,7 @@ const cardsData = [
     commentsnumber: 22,
   },
   {
-    image: "/Images/Egypt2.jpeg",
-    title: "EGYPT TANGBIA",
-    description:
-      "Denatibus et magnis dis parturient montes, nascetur ridiculus mus",
-    date: "Aug 5, 2013",
-    commentsnumber: 22,
-  },
-  {
-    image: "/Images/Egypt3.jpeg",
-    title: "EGYPT TANGBIA",
-    description:
-      "Denatibus et magnis dis parturient montes, nascetur ridiculus mus",
-    date: "Aug 5, 2013",
-    commentsnumber: 22,
-  },
-  {
     image: "/Images/OLLING1.jpeg",
-    title: "OLLING PHISOTIP",
-    description:
-      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
-    date: "Aug 5, 2013",
-    commentsnumber: 13,
-  },
-  {
-    image: "/Images/OLLING2.jpeg",
-    title: "OLLING PHISOTIP",
-    description:
-      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
-    date: "Aug 5, 2013",
-    commentsnumber: 13,
-  },
-  {
-    image: "/Images/OLLING3.jpeg",
     title: "OLLING PHISOTIP",
     description:
       "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
@@ -56,28 +24,36 @@ const cardsData = [
     commentsnumber: 8,
   },
   {
-    image: "/Images/CORTIA2.jpeg",
-    title: "CORTIA DYNA",
-    description:
-      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
-    date: "Aug 5, 2013",
-    commentsnumber: 8,
-  },
-  {
-    image: "/Images/CORTIA3.jpeg",
-    title: "CORTIA DYNA",
-    description:
-      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
-    date: "Aug 5, 2013",
-    commentsnumber: 8,
-  },
-  {
     image: "/Images/DUBAI1.jpeg",
     title: "SYSNE OF DUBAI",
     description:
       "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
     date: "Aug 5, 2013",
     commentsnumber: 7,
+  },
+  {
+    image: "/Images/Egypt2.jpeg",
+    title: "EGYPT TANGBIA",
+    description:
+      "Denatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 22,
+  },
+  {
+    image: "/Images/OLLING2.jpeg",
+    title: "OLLING PHISOTIP",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 13,
+  },
+  {
+    image: "/Images/CORTIA2.jpeg",
+    title: "CORTIA DYNA",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 8,
   },
   {
     image: "/Images/DUBAI2.jpeg",
@@ -87,6 +63,33 @@ const cardsData = [
     date: "Aug 5, 2013",
     commentsnumber: 7,
   },
+  {
+    image: "/Images/Egypt3.jpeg",
+    title: "EGYPT TANGBIA",
+    description:
+      "Denatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 22,
+  },
+
+  {
+    image: "/Images/OLLING3.jpeg",
+    title: "OLLING PHISOTIP",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 13,
+  },
+
+  {
+    image: "/Images/CORTIA3.jpeg",
+    title: "CORTIA DYNA",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 8,
+  },
+
   {
     image: "/Images/DUBAI3.jpeg",
     title: "SYSNE OF DUBAI",
@@ -112,7 +115,7 @@ $(document).ready(function () {
           ${cardData.description}
           </p>
         </div>
-        <div class="date-comments d-flex  p-4 gap-5">
+        <div class="date-comments d-flex justify-content-center p-4 gap-5">
           <div class="d-flex align-items-center gap-1">
             <i class="fa-regular fa-calendar"></i>
             <p>${cardData.date}</p>
