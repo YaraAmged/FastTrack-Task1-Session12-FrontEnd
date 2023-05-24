2ddb571911ce1e8af6a2d39cd19d89eef71e9a62
Yara Amged <Yara.Amged@itworx.com>
2023-05-24 17:20:25 +0300

    Cards Updated

--- main.js
+++ main.js
@@ -1,32 +1,130 @@
-const cardsData = [{image:"", title:"yara", description:"", date:"", commentsnumber:0}]
+const cardsData = [
+  {
+    image: "/Images/Egypt1.jpeg",
+    title: "EGYPT TANGBIA",
+    description:
+      "Denatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 22,
+  },
+  {
+    image: "/Images/Egypt2.jpeg",
+    title: "EGYPT TANGBIA",
+    description:
+      "Denatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 22,
+  },
+  {
+    image: "/Images/Egypt3.jpeg",
+    title: "EGYPT TANGBIA",
+    description:
+      "Denatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 22,
+  },
+  {
+    image: "/Images/OLLING1.jpeg",
+    title: "OLLING PHISOTIP",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 13,
+  },
+  {
+    image: "/Images/OLLING2.jpeg",
+    title: "OLLING PHISOTIP",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 13,
+  },
+  {
+    image: "/Images/OLLING3.jpeg",
+    title: "OLLING PHISOTIP",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 13,
+  },
+  {
+    image: "/Images/CORTIA1.jpeg",
+    title: "CORTIA DYNA",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 8,
+  },
+  {
+    image: "/Images/CORTIA2.jpeg",
+    title: "CORTIA DYNA",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 8,
+  },
+  {
+    image: "/Images/CORTIA3.jpeg",
+    title: "CORTIA DYNA",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 8,
+  },
+  {
+    image: "/Images/DUBAI1.jpeg",
+    title: "SYSNE OF DUBAI",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 7,
+  },
+  {
+    image: "/Images/DUBAI2.jpeg",
+    title: "SYSNE OF DUBAI",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 7,
+  },
+  {
+    image: "/Images/DUBAI3.jpeg",
+    title: "SYSNE OF DUBAI",
+    description:
+      "Penatibus et magnis dis parturient montes, nascetur ridiculus mus",
+    date: "Aug 5, 2013",
+    commentsnumber: 7,
+  },
+];
 
-$(document).ready(function(){
-const cardsWrapper = $("#cards-wrapper");
-for (let [index, cardData] of cardsData.entries()){
-const cardHTML =`
-<div class="card col-12 col-sm-3 p-0">
+$(document).ready(function () {
+  const cardsWrapper = $("#cards-wrapper");
+  for (let [index, cardData] of cardsData.entries()) {
+    const cardHTML = `
+    <div class="col-12 col-sm-3">
+<div class=" card p-0 text-center">
         <img
-          src="/Images/WhatsApp Image 2023-05-23 at 9.22.09 PM (1).jpeg"
-          class="card-img-top"
+          src="${cardData.image}"
         />
         <div class="card-body">
           <h5 class="card-title">${cardData.title}</h5>
           <p class="card-text">
-            Denatibus et magnis dis parturient montes, nascetur ridiculus mus
+          ${cardData.description}
           </p>
         </div>
-        <div class="d-flex">
-          <div class="d-flex">
+        <div class="date-comments d-flex  p-4 gap-5">
+          <div class="d-flex align-items-center gap-1">
             <i class="fa-regular fa-calendar"></i>
-            <p>Aug 5, 2013</p>
+            <p>${cardData.date}</p>
           </div>
-          <div class="d-flex">
+          <div class="d-flex align-items-center gap-1">
             <i class="fa-regular fa-comments"></i>
-            <p>22 Comments</p>
+            <p>${cardData.commentsnumber} Comments</p>
           </div>
         </div>
       </div>
+      </div>
  `;
-cardsWrapper.html(cardsWrapper.html()+cardHTML);
-}
-})
+    cardsWrapper.html(cardsWrapper.html() + cardHTML);
+  }
+});
